a82b67d89d81e1f3a35aff56b3dc8bda13dc5b53
Awez Hanegaonkar <awez.hanegaonkar@gmail.com>
2021-03-28 19:13:58 +0530

    Improved the performnce of gcd

--- AlgorithmDataStructure/NpTEL Course/GreatestCommonDivisor.py
+++ AlgorithmDataStructure/NpTEL Course/GreatestCommonDivisor.py
@@ -9,7 +9,7 @@ Metgod for finding gcd of two numbers
 ex. gcd(8,16) is 8
 """
 
-def gcdof(m,n):
+def gcd_a(m,n):
     
     fm = []
     for i in range(1, m+1):
@@ -29,4 +29,76 @@ def gcdof(m,n):
             
     return fc[-1]
 
-print(gcdof(8, 46))
+print(gcd_a(14, 63))
+
+
+# Improvement to the above code
+
+def gcd_b(m,n):
+    fc = []
+    for i in range(1, min(m,n) + 1):
+        if (m%i == 0 and n%i == 0):
+            fc.append(i)
+            
+    return fc[-1]
+
+print(gcd_b(14,63))
+
+
+# working from backward 
+
+
+def gcd_c(m,n):
+    i = min(m,n)
+    
+    while i>0:
+        if (m%i == 0 and n%i ==0):
+            return i
+        else:
+            i = i-1
+            
+print(gcd_c(14,63))
+
+
+# more advance algorithm is using recursion
+
+def gcd_d(m,n):
+    
+    if m<n:
+        (m,n) = (n,m)
+        
+    if (m%n == 0):
+        return n
+    else:
+        diff = m-n
+        return gcd_d(max(n,diff), min(n,diff))
+    
+print(gcd_d(14,63))
+    
+
+def gcd_e(m,n):
+    
+    if m<n:
+        (m,n) = (n,m)
+        
+    if m%n ==0:
+        return n
+    else:
+        return gcd_e(n, m%n)
+    
+    
+print(gcd_e(32,84))
+
+
+def gcd_f(m,n):
+    
+    if m<n:
+        (m,n) = (n,m)
+        
+    while m%n != 0:
+        (m,n) = (n, m%n)
+        
+    return n
+
+print(gcd_f(32,84))
+        
